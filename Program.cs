e044de22e836f82a87f38c260cb7fa0e142e983e
Rumbiz <rumbisdolgopa@gmail.com>
2024-05-26 21:44:56 +0300

    Добавлен исходный код на C# в файле Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите элементы массива через пробел:");
+        string[] arr = Console.ReadLine().Split(' ');
+
+        string[] filteredArr = FilterShortStrings(arr);
+        foreach (var str in filteredArr) {
+            Console.Write($"{str} ");
+        }
+    }
+
+    private static string[] FilterShortStrings(string[] arr) {
+        List<string> result = new List<string>();
+        foreach (var item in arr) {
+            if (item.Length <= 3) {
+                result.Add(item);
+            }
+        }
+        return result.ToArray();
+    }
+}
